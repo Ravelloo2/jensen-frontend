6e63e2c9d57143516b2ade52f5b42c36313ba643
Eliaz Ravello <eliaz@telia.com>
2022-01-14 12:37:11 +0000

    Rattad

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var username
 var password
 
+
 document.addEventListener("submit", (event) => {
     console.log("Submitted")
     username = document.getElementById("username").value
     password = document.getElementById("password").value
+
+    console.log(username)
+    console.log(password)
 
     const headers = new Headers()
     headers.append("Accept" , "application/json")
